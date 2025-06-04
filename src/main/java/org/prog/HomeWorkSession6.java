13d6e002d905daebbb6cfe36d0b0fbb383e990c9
olga-qa25 <romanenkoolga790@gmail.com>
2025-06-04 19:02:30 +0200

    HomeWorkSession61

--- src/main/java/org/prog/HomeWorkSession6.java
+++ src/main/java/org/prog/HomeWorkSession6.java
@@ -5,36 +5,35 @@ package org.prog;
 // 2. Create new Plane, set passengers, max apssengers and flight id
 
 public class HomeWorkSession6 {
-
-    /**
-     * ###############################
-     * ###[aliceCar@Car]##############
-     * #######s3->[]##################
-     * #####[s1->"some string"]#######
-     * ######[s2->""]#################
-     * ##################[bobCar@Car]#
-     * ###############################
-     * ###############################
-     */
-
     public static void main(String[] args) {
-        Car aliceCar = new Car();
-        Car bobCar = new Car();
+        Plane passengerCurrent = new Plane();
+        Plane passengerMax = new Plane();
+        Plane passengerId = new Plane();
 
-        aliceCar.color = "blue";
-        aliceCar.model = "3";
-        bobCar.color = "red";
+        passengerCurrent.current = 5;
+        passengerMax.max = 50;
+        passengerId.id = "AA-1001";
 
+        System.out.println("Current passengers :" + passengerCurrent.current);
+        System.out.println("Max Passengers" + " : " + passengerMax.max);
+        System.out.println("ID is " + passengerId.id);
+//        Car aliceCar = new Car();
+//        Car bobCar = new Car();
+//
+//        aliceCar.color = "blue";
+//        aliceCar.model = "3";
+//        bobCar.color = "red";
+//
 //        System.out.println(aliceCar.color);
 //        System.out.println(bobCar.color);
-
-        aliceCar.goTo();
-        bobCar.goTo();
-
-        aliceCar.refuel(25, "diesel");
-        bobCar.refuel(15, "gasoline");
-
-//        System.out.println(aliceCar.model);
+//
+//        aliceCar.goTo();
+//        bobCar.goTo();
+//
+//        aliceCar.refuel(25, "diesel");
+//        bobCar.refuel(15, "gasoline");
+//
+//       System.out.println(aliceCar.model);
 //        System.out.println(bobCar.model);
 //
 //        String s1 = "some string";
--- src/main/java/org/prog/Plane.java
+++ src/main/java/org/prog/Plane.java
@@ -1,5 +1,10 @@
 package org.prog;
 
 public class Plane {
+    public int current;
+    public int max;
+    public String id;
+
+
 
 }
