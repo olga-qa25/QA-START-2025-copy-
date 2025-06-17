792f34e3125cf3264c2eec1ec1e6f25c4ab4bb3e
oleksiiosypenko <oleksii.osypenko@epiceros.com>
2025-06-17 20:04:11 +0200

    Session 1: Java Essentials pt1

--- src/main/java/org/prog/Car.java
+++ src/main/java/org/prog/Car.java
@@ -3,14 +3,8 @@ package org.prog;
 public class Car {
 
     public String color;
-    public String model;
 
-    //TODO: Add method parameter with destination
     public void goTo() {
         System.out.println(color + " car is going somewhere");
-    }
-
-    public void refuel(int amount, String fuelType) {
-        System.out.println(color + " car is refueling " + amount + " liters of " + fuelType);
     }
 }
