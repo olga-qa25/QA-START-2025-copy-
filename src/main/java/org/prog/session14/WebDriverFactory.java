ca544f6a1b09dbc7a573ed097a5ba0278a2032f6
aosipenko <aoskiev@gmail.com>
2025-08-07 18:50:40 +0200

    Revert "Session 14: debug logs"
    This reverts commit 582502fa5d3375e6f66cc98f8e5bf460f96a9928.

--- src/main/java/org/prog/session14/WebDriverFactory.java
+++ src/main/java/org/prog/session14/WebDriverFactory.java
@@ -16,7 +16,6 @@ public class WebDriverFactory {
     @SneakyThrows
     public static WebDriver getDriver() {
         String browser = System.getProperty("browser", "local_chrome");
-        System.out.println(">>>>>>" + browser);
         switch (browser) {
             case "local_chrome":
                 return new ChromeDriver();
