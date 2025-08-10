2d03f303637170c2c7af0dac7a40314018d88e39
olga-qa25 <romanenkoolga790@gmail.com>
2025-08-10 10:46:48 +0200

    AutoHW3.1

--- src/main/java/org/prog/session1/AutomationSession1.java
+++ src/main/java/org/prog/session1/AutomationSession1.java
@@ -1,16 +1,42 @@
+//package org.prog.session1;
+//
+//public class AutomationSession1 {
+//
+//    public static void main(String[] args) {
+//
+//        Car car = new Car();
+//        Car carOdessa = new Car();
+//
+//        car.color = "red";
+//        carOdessa.color = "blue";
+//
+//        car.goToK("Kyiv");
+//        carOdessa.goToO("Odessa");
+//    }
+//}
+
 package org.prog.session1;
 
+//TODO: Add second car, make it blue, make red car got to Kyiv, blue car go to Odessa
 public class AutomationSession1 {
 
     public static void main(String[] args) {
+        Actor leadActor = new Actor();
+        Actor supportActor = new Actor();
+
+        leadActor.name = "Michael Brown";
+        supportActor.name = "Jane Brown";
+
+        leadActor.say("Hello, my name is Michael Brown");
+        supportActor.say("Hello, my name is Jane Brown");
+
+        leadActor.say("This is my first concert");
+        supportActor.say("This is my second concert");
+
+        leadActor.walkTo("edge of scene");
+        supportActor.walkTo("edge of scene");
 
         Car car = new Car();
-        Car carOdessa = new Car();
-
         car.color = "red";
-        carOdessa.color = "blue";
-
-        car.goToK("Kyiv");
-        carOdessa.goToO("Odessa");
     }
 }
--- src/main/java/org/prog/session1/Car.java
+++ src/main/java/org/prog/session1/Car.java
@@ -1,16 +1,56 @@
+//package org.prog.session1;
+//
+/// /TODO: Homework make goTo accept String as parameter and say "color car goes to destination"
+//
+//public class Car {
+//
+//    public String color;
+//
+//    public void goToK(String destination) {
+//
+//        System.out.println(color + " car is going to " + destination);
+//    }
+//    public void goToO (String destination) {
+//        System.out.println(color + " car is going to " + destination);
+//    }
+//}
 package org.prog.session1;
 
-//TODO: Homework make goTo accept String as parameter and say "color car goes to destination"
-
+//TODO: HW4: add String model and make equals & hashCode work by model and color
 public class Car {
 
     public String color;
+    public String model;
 
-    public void goToK(String destination) {
+    public void goTo() {
 
-        System.out.println(color + " car is going to " + destination);
+        System.out.println(color + " car is going somewhere");
     }
-    public void goToO (String destination) {
-        System.out.println(color + " car is going to " + destination);
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Car) {
+            Car car = (Car) obj;
+            return this.color.equals(car.color) &&
+                    this.model.equals(car.model);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return color.hashCode() + model.hashCode();
+    }
+
+    @Override
+    public String toString() {
+
+        return "Car : " + color;
+    }
+
+    public String getColor() {
+
+        return color;
     }
 }
