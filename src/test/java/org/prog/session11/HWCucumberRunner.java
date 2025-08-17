7c33449b383a46953ad45580f0f049eedff8182b
olga-qa25 <romanenkoolga790@gmail.com>
2025-08-17 10:55:43 +0200

    AutoHW12.7

--- src/test/java/org/prog/session11/HWCucumberRunner.java
+++ src/test/java/org/prog/session11/HWCucumberRunner.java
@@ -24,7 +24,7 @@ public class HWCucumberRunner extends AbstractTestNGCucumberTests {
     public void beforeSuite() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         HWDBStep.connection = DriverManager.getConnection(
-                "jdbc:mysql://db:3306/db-1", "root", "password");
+                "jdbc:mysql://mysql-db:3306/db", "root", "password");
     }
 
     @AfterSuite
